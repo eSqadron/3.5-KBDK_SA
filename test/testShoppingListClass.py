21a56325308804b87599abbd60072b4c692ccc67
eSqadron <kubam0906@gmail.com>
2020-06-21 18:53:09 +0200

    dokończenie testów jednostkowych dla konstruktora klasy ShoppingList

--- test/testShoppingListClass.py
+++ test/testShoppingListClass.py
@@ -1,11 +1,21 @@
 import unittest
-from main import ShoppingList
+from main import ShoppingList, ListNameLengthError, ListNameAlreadyTakenError
 
-class MyTestCase(unittest.TestCase):
-    def test_constructor_name(self):
+
+class test_constructor(unittest.TestCase):
+    def test_name_setting(self):
         sl = ShoppingList("name")
         self.assertEqual(sl.name_, "name")
 
+    def test_name_setting_length_short(self):
+        sl = ShoppingList("name")
+
+    def test_name_setting_length_long(self):
+        self.assertRaises(ListNameLengthError, ShoppingList("namefffffffffffffffffffffffffff"))
+
+    def test_name_not_taken(self):
+        listsList = [ShoppingList("name")]
+        self.assertRaises(ListNameAlreadyTakenError, ShoppingList("name"))
 
 if __name__ == '__main__':
     unittest.main()
