334bd04eab22e85b0c446d1de298df4ce7ffb500
Kacper Kozaczko <kozaczko@student.agh.edu.pl>
2020-06-23 23:22:27 +0200

    dodanie skompresowanej wersji modułu

--- lib.py
+++ lib.py
@@ -2,15 +2,84 @@
 # -*- coding: utf-8 -*-
 
 import tkinter as tk
-from lib_var import f1, listsList
-from shopping_list_class import ShoppingList
+from typing import Optional
 
-##biblioteka zawierająca funkcje
 
-# Funkcje operujące na liście wszystkich list zakupów (działające na lewej połówce ekranu, przypsiane do f1)
+class ShoppingList:
+    def __init__(self, name):
+        self.name_ = name  # nazwa tej konkretnej listy
+        self.list_ = []  # zawartość tej listy (str)
+        self.entryFields_ = []  # lista z polami Entry Field wyświetlanymi po prawej stronie
+        self.newButton = None  # zmienna w której zapisany jest przycisk dodającegy nowe pole do tej listy
+        self.newEntry = None  # zmienna w której zapisane jest pole tekstowe Entry Field do którego należy podać nazwę nowego pola
+
+    # metoda wypisująca zawartość zmiennej list_
+    def printList(self):
+        global currentlyOpenedList  # aby była używana globalna wersja zmiennej
+        if currentlyOpenedList is not None: currentlyOpenedList.closeList()  # zamyka obecnie otwartą listę zanim otworzy nową
+        if currentlyOpenedList is None:  # może coś wyświetlić tylko jeżeli nic nie jest wyświetlone
+            currentlyOpenedList = self  # przypisuje siebie jako otawrta liste
+            while "" in self.list_: self.list_.remove("")  # czyszczenie listy z pustych elementów
+            # pętla wypisująca wszystkie wartości z list_ do poszczególnych pól entry
+            for j in self.list_:
+                w = tk.Entry(  # tworzy pola typu entry w których są nazwy produktów
+                    f2,
+                    width=25
+                )
+                w.insert(0, j)
+                w.pack(side=tk.TOP)
+                self.entryFields_.append(w)  # dodaje te pola do listy aby potem dało się je usunac
+            ##############
+
+            # pole tekstowe odpowiedzialne za zebranie nazwy nowego porduktu
+            self.newEntry = tk.Entry(
+                f2,
+                width=25
+            )
+            self.newEntry.insert(0, "Podaj nazwę nowego produktu")
+            self.newEntry.pack(side=tk.BOTTOM)
+            ############
+
+            # przycisk odpowiedzialny za dodawnia nowego produktu
+            self.newButton = tk.Button(
+                f2,
+                width=25,
+                height=1,
+                text="dodaj produkt",
+                command=lambda: self.addProduct(self.newEntry.get())
+            )
+            self.newButton.pack(side=tk.BOTTOM)
+            ##########
+
+    # metoda dodająca nowy produkt do listy
+    def addProduct(self, productName):
+        self.list_.append(productName)
+        if self == currentlyOpenedList:
+            self.printList()
+
+    # metoda zamykająca obecnie otwartą listę
+    def closeList(self):
+        global currentlyOpenedList
+        isShowed = False
+        self.saveList()
+        for i in self.entryFields_:
+            i.destroy()
+        self.newButton.destroy()
+        self.newEntry.destroy()
+        self.entryFields_.clear()
+        currentlyOpenedList = None
+
+    # metoda zapisująca obecnie otwartą listę do listy list
+    def saveList(self):
+        global currentlyOpenedList
+        for i in range(len(self.entryFields_)):
+            self.list_[i] = (self.entryFields_[i]).get()
+
+
+#########################################
+# FUCTIONS
 
 # funkcja tworząca przycisk służący do tworzenia nowych list i pole tekstowe typu entry gdzie trzeba podać nazwę nowej listy
-
 def createNewListButton():
     global newListButton  # aby do obu pól można było się odwoływać gdzie indziej muyszą byc globalne
     global entry
@@ -96,3 +165,41 @@ def saveToFile():
 # funkcja używana w opcji menu górnego poziomego "Wczytaj", służąca wczytywaniu z pliku
 def readFromFile():
     pass
+
+
+############################
+# variables
+
+currentlyOpenedList: Optional[ShoppingList] = None  # obecnie otwarta lista
+root = tk.Tk() #głowne okno
+
+# tworzę framey. w lewej (1) ramce zamieszczam kolejno przyciski z listami. W prawej po kliknięciu pojawia się konkretna lista
+f1 = tk.Frame(
+    root,
+    width=25,
+    height=100,
+    bd=1
+)
+f2 = tk.Frame(
+    root,
+    width=25,
+    height=100,
+    bd=5
+)
+# tworzenie przycisku zamykającego otwartą listę
+closeListButton = tk.Button(
+    f2,  # przypisanie do prawego framea
+    text="zamknij obecną listę",
+    width=25,
+    height=1,
+    command=lambda: currentlyOpenedList.closeList if currentlyOpenedList is not None else None  # Nie działa?
+)
+
+listsList = []  # lista list - lista zawierająca listy zakupów
+
+
+#Menu górne
+menu = tk.Menu(root)
+root.config(menu=menu)
+menu.add_command(label="Zapisz", command=saveToFile)
+menu.add_command(label="Wczytaj", command=readFromFile)
