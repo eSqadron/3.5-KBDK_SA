2bf9a9ca5aeaa96c87151f54eb4e4640a44982af
Kacper Kozaczko <kozaczko@student.agh.edu.pl>
2020-06-24 18:57:43 +0200

    dodanie folderu test

---
+++ test/testShoppingListClass.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from lib import *
+import unittest
+from main import ShoppingList, ListNameLengthError, ListNameAlreadyTakenError
+
+
+class test_constructor(unittest.TestCase):
+    def test_name_setting(self):
+        sl = ShoppingList("name")
+        self.assertEqual(sl.name_, "name")
+
+    def test_name_setting_length_short(self):
+        sl = ShoppingList("name")
+
+    def test_name_setting_length_long(self):
+        self.assertRaises(ListNameLengthError, ShoppingList("namefffffffffffffffffffffffffff"))
+
+    def test_name_not_taken(self):
+        listsList = [ShoppingList("name")]
+        self.assertRaises(ListNameAlreadyTakenError, ShoppingList("name"))
+
+if __name__ == '__main__':
+    unittest.main()
