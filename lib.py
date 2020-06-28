a0e17b2e1d6204eb12861ad859ac8fb605c3c9d3
bradzio225 <62242230+bradzio225@users.noreply.github.com>
2020-06-29 00:49:19 +0200

    dodanie biblioteki

--- lib.py
+++ lib.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.filedialog
 from typing import Optional
 from ShoppingListErrors import ListAmountError, ListElementsAmountError, ListNameAlreadyTakenError, ListNameLengthError
 
